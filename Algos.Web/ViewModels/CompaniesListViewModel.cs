0bddca7b994afc67ef8563b09f02cbcf04c22bbd
Etsellon <etsellondev@gmail.com>
2025-06-27 01:38:23 +0300

    Добавлен Контроллер, маппер и модели представлений

---
+++ Algos.Web/ViewModels/CompaniesListViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Algos.Web.ViewModels
+{
+    public class CompaniesListViewModel
+    {
+        public IEnumerable<CompanyViewModel> Companies { get; set; } = [];
+        public int CurrentPage { get; set; }
+        public int PageSize { get; set; }
+        public int TotalItems { get; set; }
+        public int TotalPages => (int)Math.Ceiling((double)TotalItems / PageSize);
+    }
+}
