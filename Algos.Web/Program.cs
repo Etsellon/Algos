f0bb1ae8d591c71a5f82154c3d76300062f3f8d9
Etsellon <etsellondev@gmail.com>
2025-06-27 01:39:03 +0300

    Добавлен набор стилей custom, докер файл для бд, настроен запуск программы

--- Algos.Web/Program.cs
+++ Algos.Web/Program.cs
@@ -1,7 +1,22 @@
+using Algos.Application.Services;
+using Algos.Core.Abstractions.Interfaces;
+using Algos.DataAccess;
+using Algos.DataAccess.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ApplicationDbContext>(
+    options =>
+    {
+        options.UseNpgsql(builder.Configuration.GetConnectionString(nameof(ApplicationDbContext)));
+    });
+
+builder.Services.AddScoped<ICompanyRepository, CompanyRepository>();
+builder.Services.AddScoped<ICompanyServices, CompanyServices>();
 
 var app = builder.Build();
 
@@ -22,6 +37,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Company}/{action=Index}/{id?}");
 
 app.Run();
